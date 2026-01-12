c5122b09334830810d43e5fd6b3b82da51fad322
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-12 13:57:22 +0300

    feat: add rating to product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Image, Text } from "@chakra-ui/react";
+import { Button, Card, Image, RatingGroup } from "@chakra-ui/react";
 import type Product from "@/entities/Product";
 
 interface Props {
@@ -17,7 +17,7 @@ const ProductCard = ({ product }: Props) => {
         }}
         transition="transform 0.15s ease-in-out"
       >
-        <Image src={product.images[0]} alt={product.images[0]} />
+        <Image src={product.thumbnail} alt={product.images[0]} />
         <Card.Body gap="2">
           <Card.Title fontSize={16} fontWeight="400">
             {product.title}
@@ -33,10 +33,28 @@ const ProductCard = ({ product }: Props) => {
           >
             $450
           </Text> */}
+          <RatingGroup.Root
+            allowHalf
+            count={5}
+            value={product.rating}
+            size="sm"
+            colorPalette="red"
+          >
+            <RatingGroup.HiddenInput />
+            <RatingGroup.Control />
+          </RatingGroup.Root>
         </Card.Body>
         <Card.Footer gap="2">
-          <Button variant="solid">Buy now</Button>
-          <Button variant="ghost">Add to cart</Button>
+          <Button variant="solid">Add to Cart</Button>
+          <Button
+            variant="ghost"
+            border="1px solid black"
+            _dark={{
+              border: "1px solid white",
+            }}
+          >
+            Read More
+          </Button>
         </Card.Footer>
       </Card.Root>
     </>
